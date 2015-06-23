37a271b86bd1a705d2b2c575562faf400c8d62d1
michlandry <mlandryviolin@gmail.com>
2015-06-23 17:27:23 -0400

    initial commit

---
+++ src/hamsterofdark/BlogServlet.java
@@ -0,0 +1,22 @@
+package hamsterofdark;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class BlogServlet extends HttpServlet {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws IOException, ServletException {
+//		resp.setContentType("text/plain");
+//		resp.getWriter().println("Hello, world");
+		
+//		RequestDispatcher view = request.getRequestDispatcher("html/spring.html");
+		RequestDispatcher view = request.getRequestDispatcher("blogPage.jsp");
+		view.forward(request, response);
+
+		
+	}
+}
